ddaf7ef7604211fd3b36bc8e28fcdf3f593c853c
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2023-07-08 01:32:56 +0100

    tests: Add tests for parsing ret statements

--- src/tests/parser.rs
+++ src/tests/parser.rs
@@ -4,6 +4,8 @@ use crate::core::tokens::{Token, TokenType};
 
 use test_case::test_case;
 
+use super::ast;
+
 #[test_case("let x = 5;", vec!["x"], 1; "Simple parser test with a single let assignment")]
 #[test_case("let x = 5; let y = 10;", vec!["x", "y"], 2; "Simple parser test with two let assignments on a single line")]
 #[test_case("
@@ -40,6 +42,36 @@ fn test_let_statements(
         match statement {
             Statement::Assignment(let_statement) => {
                 assert_eq!(let_statement.identifier.name, expected_identifier_name);
+            }
+            _ => {}
+        }
+    }
+}
+
+#[test_case("return 5;", 1; "Simple parser test with a single return")]
+#[test_case("
+return 5;
+return 10;
+return 993322;
+", 3;
+"Multiple return statements;")]
+fn test_return_statements(input: &str, expected_num_statements: usize) {
+    let mut parser = Parser::new(input).unwrap();
+    let program = parser.parse_program();
+
+    assert_eq!(
+        program.statements.len(),
+        expected_num_statements,
+        "Program should contain {expected_num_statements} statements"
+    );
+
+    for i in 0..expected_num_statements {
+        let current_statement = program.statements.get(i).unwrap();
+        assert!(std::matches!(current_statement, ast::Statement::Return(_)));
+
+        match current_statement {
+            ast::Statement::Return(rs) => {
+                assert_eq!(rs.token.r#type, TokenType::Return);
             }
             _ => {}
         }
